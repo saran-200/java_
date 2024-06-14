011781a84aff309685957a6a41868bee12eff354
SANDY SANTHOSH <saran.r.it.mec@gmail.com>
2024-06-14 10:01:16 +0530

    programs

--- sara.java
+++ sara.java
@@ -1,7 +1,7 @@
 class sara{
     public static void main(String[] args) {
-        int nu[]={1,2,3,4,5};
-        for(int n:nu)System.out.println(n);
-        
+        int i ='A';
+        int j = ++i +2;
+        System.out.println((char)j);
     }
 }
